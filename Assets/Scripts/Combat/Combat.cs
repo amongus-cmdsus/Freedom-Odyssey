8091b7a08997baaa95fc54f2cdbb7a409292fcb7
MojoSharpeye <genshinreroll00000@gmail.com>
2024-06-08 11:30:13 +1000

    More Changes

--- Assets/Scripts/Combat/Combat.cs
+++ Assets/Scripts/Combat/Combat.cs
@@ -54,8 +54,6 @@ public class Combat : MonoBehaviour
 
         if(combatMode == 1){
             playerMovement.enabled = false;
-
-            ground.GetComponent<Renderer>().material.Offset = transform.position;
         }
     }
 }
